ab2ca6e6d076c9f5ae5e758b0a01490534ac9aca
Brent Buchholz <entomdt@gmail.com>
2021-04-24 16:50:58 +0000

    Refactor first example into component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //import './App.css';
 //import 'bootstrap/dist/css/bootstrap.css';
 import './custom.scss';
+import ContainerFirst from './ContainerFirst';
 
 function App() {
   return (
@@ -8,25 +9,7 @@ function App() {
       <header className="App-header">
         React Header
       </header>
-      <div class="container">
-        {/* Row with two equal columns */}
-        <div class="row">
-            <div class="col-md-6">Column left</div>
-            <div class="col-md-6">Column right</div>
-        </div>
-        
-        {/* Row with two columns divided in 1:2 ratio */}
-        <div class="row">
-            <div class="col-md-4">Column left</div>
-            <div class="col-md-8">Column right</div>
-        </div>
-        
-        {/* Row with two columns divided in 1:3 ratio */}
-        <div class="row">
-            <div class="col-md-3">Column left</div>
-            <div class="col-md-9">Column right</div>
-        </div>
-    </div>
+      <ContainerFirst />
     </div>
   );
 }
---
+++ src/ContainerFirst.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class ContainerFirst extends React.Component {
+    render(){
+        return (
+            <div class="container">
+                {/* Row with two equal columns */}
+                <div class="row">
+                    <div class="col-md-6">Column left</div>
+                    <div class="col-md-6">Column right</div>
+                </div>
+                
+                {/* Row with two columns divided in 1:2 ratio */}
+                <div class="row">
+                    <div class="col-md-4">Column left</div>
+                    <div class="col-md-8">Column right</div>
+                </div>
+                
+                {/* Row with two columns divided in 1:3 ratio */}
+                <div class="row">
+                    <div class="col-md-3">Column left</div>
+                    <div class="col-md-9">Column right</div>
+                </div>
+            </div>
+        );        
+    }
+}
+
+export default ContainerFirst;
